156b7f8ae22bec03108139b16cdbe5a007663545
Brandon <brandonbartram@outlook.com>
2023-07-29 23:06:34 +0100

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,176 @@
+'use client'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import { io } from 'socket.io-client'
+
+let socket
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [showSetup, setShowSetup] = useState(true)
+	const [message, setMessage] = useState('')
+	const [username, setUsername] = useState('')
+	const [allMessages, setAllMessages] = useState([])
+	const [allUsers, setAllUsers] = useState([])
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	useEffect(() => {
+		socketInitializer()
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+		return () => {
+			socket.disconnect()
+		}
+	}, [])
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+	async function socketInitializer() {
+		await fetch('/api/socket')
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+		socket = io(undefined, {
+			path: '/api/socket_io',
+		})
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+		socket.on('receive-message', (data) => {
+			setAllMessages((pre) => [...pre, data])
+		})
+
+		socket.on('retrieve-users', (data) => {
+			setAllUsers((pre) => [...pre, data])
+		})
+	}
+
+	function handleSetup(e) {
+		e.preventDefault()
+
+		console.log('setting user')
+
+		setShowSetup(false)
+	}
+
+	function handleSubmit(e) {
+		e.preventDefault()
+
+		console.log('emitted')
+
+		socket.emit('send-message', {
+			username,
+			message,
+		})
+
+		setMessage('')
+	}
+
+	return (
+		<main className="min-h-screen">
+			{showSetup ? (
+				<div className="fixed flex items-center bg-black/80 h-screen w-full">
+					<form
+						onSubmit={handleSetup}
+						className="flex flex-col gap-4 bg-white rounded-lg mx-auto p-12 text-center w-1/2"
+					>
+						<h2 className="text-black font-bold text-2xl uppercase">
+							Chatroom
+						</h2>
+						<input
+							className="p-4 border border-black text-black"
+							type="text"
+							placeholder="username"
+							value={username}
+							onChange={(e) => setUsername(e.target.value)}
+						/>
+						<div className="text-black flex flex-col gap-2">
+							<p className="font-bold">Choose a color: </p>
+							<div className="flex flex-row justify-center gap-2 p-2">
+								<button className="bg-black p-4"></button>
+								<button className="bg-red-500 p-4"></button>
+								<button className="bg-blue-500 p-4"></button>
+								<button className="bg-green-500 p-4"></button>
+								<button className="bg-yellow-500 p-4"></button>
+								<button className="bg-orange-500 p-4"></button>
+								<button className="bg-purple-500 p-4"></button>
+								<button className="bg-cyan-500 p-4"></button>
+							</div>
+						</div>
+						<button className="flex justify-center p-4 bg-yellow-500">
+							Join
+						</button>
+					</form>
+				</div>
+			) : null}
+			<div className="grid grid-cols-12 bg-black h-screen">
+				<div className="col-span-4 bg-slate-600 p-6">
+					<svg
+						viewBox="0 0 15 15"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+						width="21"
+						height="21"
+					>
+						<path
+							clip-rule="evenodd"
+							d="M10.5 3.498a2.999 2.999 0 01-3 2.998 2.999 2.999 0 113-2.998zm2 10.992h-10v-1.996a3 3 0 013-3h4a3 3 0 013 3v1.997z"
+							stroke="currentColor"
+							stroke-linecap="square"
+						></path>
+					</svg>
+					{allUsers.map(({ username }, index) => (
+						<div key={index}>
+							<p>{username}</p>
+						</div>
+					))}
+				</div>
+				<div className="col-span-8 flex flex-col items">
+					<div className="bg-slate-900 w-full h-full p-6">
+						<svg
+							className="mb-2"
+							viewBox="0 0 15 15"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+							width="21"
+							height="21"
+						>
+							<path
+								d="M11.5 13.5l.157-.475-.218-.072-.197.119.258.428zm2-2l-.421-.27-.129.202.076.226.474-.158zm1 2.99l-.157.476a.5.5 0 00.631-.634l-.474.159zm-3.258-1.418c-.956.575-2.485.919-3.742.919v1c1.385 0 3.106-.37 4.258-1.063l-.516-.856zM7.5 13.99c-3.59 0-6.5-2.909-6.5-6.496H0a7.498 7.498 0 007.5 7.496v-1zM1 7.495A6.498 6.498 0 017.5 1V0A7.498 7.498 0 000 7.495h1zM7.5 1C11.09 1 14 3.908 14 7.495h1A7.498 7.498 0 007.5 0v1zM14 7.495c0 1.331-.296 2.758-.921 3.735l.842.54C14.686 10.575 15 8.937 15 7.495h-1zm-2.657 6.48l3 .99.314-.949-3-.99-.314.949zm3.631.357l-1-2.99-.948.316 1 2.991.948-.317z"
+								fill="currentColor"
+							></path>
+						</svg>
+						{allMessages.map(({ username, message }, index) => (
+							<div key={index}>
+								<p className="text-lg pt-2">
+									<span className="text-red-700">{username}</span>: {message}
+								</p>
+							</div>
+						))}
+					</div>
+					<form
+						onSubmit={handleSubmit}
+						className="flex bg-gray-600 w-full h-64"
+					>
+						<textarea
+							name="message"
+							className="w-full h-full p-6 text-black"
+							type="text"
+							placeholder="enter message.."
+							value={message}
+							onChange={(e) => setMessage(e.target.value)}
+						/>
+						<button
+							type="submit"
+							className="bg-gray-600 text-center items-center flex p-6 font-bold uppercase"
+						>
+							<svg
+								viewBox="0 0 15 15"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+								width="21"
+								height="21"
+							>
+								<path
+									d="M14.5.5l.46.197a.5.5 0 00-.657-.657L14.5.5zm-14 6l-.197-.46a.5.5 0 00-.06.889L.5 6.5zm8 8l-.429.257a.5.5 0 00.889-.06L8.5 14.5zM14.303.04l-14 6 .394.92 14-6-.394-.92zM.243 6.93l5 3 .514-.858-5-3-.514.858zM5.07 9.757l3 5 .858-.514-3-5-.858.514zm3.889 4.94l6-14-.92-.394-6 14 .92.394zM14.146.147l-9 9 .708.707 9-9-.708-.708z"
+									fill="currentColor"
+								></path>
+							</svg>
+						</button>
+					</form>
+				</div>
+			</div>
+		</main>
+	)
 }
